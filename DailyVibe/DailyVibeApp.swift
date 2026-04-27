19b6d6fe501f9018c004d4f82338de8e01705979
Daulet Ashikbayev <dxulet@gmail.com>
2026-04-27 18:02:46 +0500

    chore: scaffold DailyVibe iOS project

---
+++ DailyVibe/DailyVibeApp.swift
@@ -0,0 +1,18 @@
+//
+//  DailyVibeApp.swift
+//  DailyVibe
+//
+//  Created by Daulet Ashikbayev on 27.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct DailyVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
